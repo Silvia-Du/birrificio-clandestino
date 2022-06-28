5b71e9026d2aee5f89b882f9783f90214e67d4bf
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-06-28 17:12:37 +0200

    Header link active completo

--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -1,10 +1,10 @@
 <header>
     <nav>
         <ul>
-            <li><a class="active" href="#">Home</a></li>
-            <li><a href="#">Chi Siamo</a></li>
-            <li><a href="#">Contatti</a></li>
-            <li><a href="#">Le Nostre Birre</a></li>
+            <li><a class="{{ Route::currentRouteName() === 'home' ? 'active' : ''  }}" href="{{ route('home') }}">Home</a></li>
+            <li><a class="{{ Route::currentRouteName() === 'x' ? 'active' : ''  }}" href="#">Chi Siamo</a></li>
+            <li><a class="{{ Route::currentRouteName() === 'x' ? 'active' : ''  }}" href="#">Contatti</a></li>
+            <li><a class="{{ Route::currentRouteName() === 'birre' ? 'active' : ''  }}" href="{{ route('birre') }}">Le Nostre Birre</a></li>
         </ul>
     </nav>
 </header>
