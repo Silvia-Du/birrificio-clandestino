aec67b230414f6c0783c1cf836166842c9dde4a9
IvanRusso91 <iv.russo91@gmail.com>
2022-07-01 16:13:25 +0200

    Store completo

--- app/Beer.php
+++ app/Beer.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Beer extends Model
 {
-    //
+    protected $fillable =[
+        'name',
+        'company',
+        'type',
+        'alcohol_content',
+        'description',
+        'image',
+        'hops',
+        'gluteen_free',
+    ];
 }
