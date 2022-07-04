8cbd2486db4e65a885e3c8b010790c7931b2053b
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-07-04 12:44:03 +0200

    Aggiunto validazione edit

--- app/Http/Controllers/BeerCrudController.php
+++ app/Http/Controllers/BeerCrudController.php
@@ -119,6 +119,32 @@ class BeerCrudController extends Controller
      */
     public function update(Request $request, Beer $beer)
     {
+        $request->validate([
+            "name" => "required|max:255|min:3",
+            "company" => "required|max:255|min:3",
+            "type" => "required|max:255|min:3",
+            "alcohol_content" => "required|numeric|max:100|min:0",
+            "description" => "required|min:10",
+            "hops" => "numeric"
+            ],
+            [
+                "name.required" => "Il campo Nome è obbligatorio!",
+                "name.max" => "Il nome non può essere più lungo di :max caratteri!",
+                "nome.min" => "Il nome deve avere almeno :min caratteri!",
+                "company.required" => "Il campo Company è obbligatorio!",
+                "company.max" => "Il nome della company non può essere più lungo di :max caratteri!",
+                "company.min" => "Il nome della company deve avere almeno :min caratteri!",
+                "type.required" => "Il campo Type è obbligatorio!",
+                "type.max" => "Il tipo non può essere più lungo di :max caratteri!",
+                "type.min" => "Il tipo deve avere almeno :min caratteri!",
+                "alcohol_content.required" => "Il campo Alcohol Content è obbligatorio!",
+                "alcohol_content.numeric" => "Il campo Alcohol Content deve essere un numero!",
+                "alcohol_content.max" => "L'alcohol content non può essere superiore a :max!",
+                "alcohol_content.min" => "L'alcohol content non può essere inferiore a :min!",
+                "description.required" => "La descrizione è un campo obbligatorio!",
+                "description.min" => "La descrizione deve avere almeno :min caratteri!",
+                "hops.numeric" => "Il campo Hops deve essere un numero!"
+            ]);
         $data = $request->all();
 
         $beer->update($data);
